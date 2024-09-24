3357e1ec3cbc4a4ccd1f2b08729b70d0b7388da8
pinhojuliana <studyjulianaa@gmail.com>
2024-09-24 14:24:05 -0300

    Implementa Comparable na classe Cliente

--- src/main/java/conta_digital/cliente/Cliente.java
+++ src/main/java/conta_digital/cliente/Cliente.java
@@ -1,18 +1,28 @@
 package conta_digital.cliente;
 
+import conta_digital.conta.Conta;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-public class Cliente {
+public class Cliente implements Comparable<Cliente>{
     protected String nome;
     protected LocalDate dataCadastro;
     protected String email;
+    protected List<Conta> contas;
 
     public Cliente(String nome, String email){
         this.nome = nome;
         this.email = email;
         this.dataCadastro = LocalDate.now();
+        this.contas = new ArrayList<>();
+    }
+
+    @Override
+    public int compareTo(Cliente c) {
+        return this.nome.compareTo(c.nome);
     }
 }
